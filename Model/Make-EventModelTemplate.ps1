009149b0969dff1c1991ada55b9b899e0f97eb48
Matt <mattsknox@gmail.com>
2020-11-22 09:16:23 -0500

    Quick powershell script for generating the event template files

---
+++ Model/Make-EventModelTemplate.ps1
@@ -0,0 +1,18 @@
+﻿$location = Get-Location
+
+
+$eventName = "Shutdown"
+
+$fileContents = @"
+namespace EliteHelper
+{
+    public class $eventName`Event : JournalEvent
+    {
+        //TODO: this
+    }
+}
+"@
+
+$filepath = "$location\Event\$eventName`Event.cs"
+
+[System.IO.File]::WriteAllText($filepath, $fileContents)
